489aaf5d0d9bfb2c1858b21db95fb6ca968e77f1
Celine <cmdev@outlook.fr>
2025-12-29 10:38:36 +0100

    Add skill to data

---
+++ _next/static/chunks/app/page-4902abc2e90ee507.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4432:function(n,e,u){Promise.resolve().then(u.t.bind(u,5878,23))}},function(n){n.O(0,[878,971,117,744],function(){return n(n.s=4432)}),_N_E=n.O()}]);
